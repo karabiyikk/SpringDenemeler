ed427dcf483ae812ef31848ca0df0d43b6bb47a5
Alper Karabıyık <alper4919@gmail.com>
2023-05-24 17:27:03 +0300

    first commit (Deneme)

---
+++ src/test/java/com/SpringBootNew/SpringBotDenemeler/SpringBotDenemelerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringBootNew.SpringBotDenemeler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBotDenemelerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
