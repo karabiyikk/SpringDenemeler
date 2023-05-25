99770937e874548bb5257c243aadd45f09bdf34b
Alper Karabıyık <alper4919@gmail.com>
2023-05-25 12:54:00 +0300

    DevTools Aktif Etmek

---
+++ src/main/java/com/karabiyik/utils/ConfigProgramming.java
@@ -0,0 +1,10 @@
+package com.karabiyik.utils;
+
+public class ConfigProgramming {
+    /*DevTools == LiveReload
+    file -> setings -> compiler -> Build project automatic
+    file -> Advanced Settings -> Allow auto-make...
+
+
+    */
+}
