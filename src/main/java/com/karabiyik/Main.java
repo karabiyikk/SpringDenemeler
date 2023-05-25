3deb99464f6983f3b1f7a09c1d12b71bfcd7b639
Alper Karabıyık <alper4919@gmail.com>
2023-05-25 17:54:46 +0300

    thymeleaf css,html,model

--- src/main/java/com/karabiyik/Main.java
+++ src/main/java/com/karabiyik/Main.java
@@ -3,7 +3,10 @@ package com.karabiyik;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class,
+        org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration.class
+})
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
---
+++ src/main/java/com/karabiyik/controller/ThymeleafController.java
@@ -0,0 +1,22 @@
+package com.karabiyik.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ThymeleafController {
+
+    @GetMapping("/thymeleaf1")
+    public String getTymeleaf1(Model model) {
+        model.addAttribute("key_model", "modelden geldi");
+        model.addAttribute("key_model2", "model2'den geldi");
+
+        return "thymeleaf1";
+    }
+
+    @GetMapping("/thymeleaf2")
+    public String getThymeleaf2Model() {
+        return "thymeleaf1";
+    }
+}
