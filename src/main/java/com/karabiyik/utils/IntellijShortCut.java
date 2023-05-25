fc5f2aeb5d2a784b56b2f4ab558e899b3722f77e
Alper Karabıyık <alper4919@gmail.com>
2023-05-25 12:53:28 +0300

    kısayollar

---
+++ src/main/java/com/karabiyik/utils/IntellijShortCut.java
@@ -0,0 +1,10 @@
+package com.karabiyik.utils;
+
+public class IntellijShortCut {
+    //Setting == ctrl+alt+s
+    //Alt+insert == constructor,toString,getter,setter
+    //ctrl+/ == single comment
+    //ctrl+shift+/ == multiple comment
+
+
+}
